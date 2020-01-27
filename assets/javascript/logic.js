f582e11a1be756c78087348da1f8b85c7aa760a1
Mohammad Chowdhury <aktherjaved@yahoo.com>
2020-01-26 22:20:31 -0500

    Update date profile

---
+++ assets/javascript/logic.js
@@ -0,0 +1,8 @@
+function copyEmail(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
